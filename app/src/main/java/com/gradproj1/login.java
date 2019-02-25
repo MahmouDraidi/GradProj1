496a7d8fff7bb42ea5cdd055e322c114f401824c
MahmouDraidi <mahmoud.draidi1997@hotmail.com>
2019-02-25 13:06:31 +0200

    sisxth commit 26/2 correction

--- app/src/main/java/com/gradproj1/login.java
+++ app/src/main/java/com/gradproj1/login.java
@@ -1,105 +1,183 @@
 package com.gradproj1;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.concurrent.TimeUnit;
+import java.util.zip.Inflater;
 
 public class login extends AppCompatActivity {
 
     Button loginButton;
-    boolean mLocationPermissionGranted=false;
-    TextView moblile_num_textview;
+    boolean mLocationPermissionGranted = false;
+    TextView mobile_num_editText;
+    TextView signUpTextView;
+    SharedPreferences SP;
+    LinearLayout LinLay1;
     FirebaseFirestore db;
-
+    String entered_number = "";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        SharedPreferences SP=getSharedPreferences("mobile_number", MODE_PRIVATE);
-        final SharedPreferences.Editor SPE=SP.edit();
-       // FirebaseApp.initializeApp(this);
 
-        //if user already logged in then continue to app
-        if(!SP.getString("number","").equals("")){
-            startActivity(new Intent(this,MainActivity.class));
-            getLocationPermission();
-            return;
-        }
-
-        loginButton=(Button) findViewById(R.id.loginButton);
-        moblile_num_textview=(TextView)findViewById(R.id.mobile_number);
-
-        //continue to app if location permission is garanteed
+        SP = getSharedPreferences("mobile_number", MODE_PRIVATE);
         getLocationPermission();
 
+        if (!SP.getString("number", "").equals("")) {
+            //TODO redirect to user or driver
+            startActivity(new Intent(this, userMap.class));
+            finish();
+        }
 
-        //((TextView)findViewById(R.id.textView5)).setText(SP.getString("number",""));
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        mobile_num_editText = (TextView) findViewById(R.id.mobile_number);
+        signUpTextView = (TextView) findViewById(R.id.signUp);
+        LinLay1 = (LinearLayout) findViewById(R.id.linLay);
+        db = FirebaseFirestore.getInstance();
+
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(!moblile_num_textview.getText().toString().equals(""))
-                move(SPE);
+            public void onClick(final View view) {
+                if (mobile_num_editText.getText().toString().length() != 10)
+                    toastMessage("Please enter valid number");
+                else {
+                    entered_number = "+97" + mobile_num_editText.getText().toString();
+                    defineUser(entered_number);
+                }
             }
         });
+        //end of button onClick listener
+    }
+
+    public void defineUser(final String mobNumber) {
+        db.collection("users").document(mobNumber).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            user User = documentSnapshot.toObject(user.class);
+
+                            SP.edit().putString("number", User.getMobileNumber()).apply();
+                            SP.edit().putString("line", User.getLine()).apply();
+                            SP.edit().putString("type", "user").apply();
+                            SP.edit().putString("name", User.getName()).apply();
+                            SP.edit().putString("PIN", User.getPIN()).apply();
+                            move();
+
+                        } else {
+                            defineDriver(mobNumber);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        toastMessage("Something went wrong!");
+                    }
+                });
+
 
     }
 
-    public void move(SharedPreferences.Editor SPE){
+    public void defineDriver(String mobNum) {
 
-        SPE.putString("number",moblile_num_textview.getText().toString());
-        SPE.apply();
+        db.collection("drivers").document(mobNum).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            toastMessage("Driver is found");
+                            driver Driver = documentSnapshot.toObject(driver.class);
 
-        Intent i=new Intent(this,MainActivity.class);
+                            SP.edit().putString("number", Driver.getMobileNum()).apply();
+                            SP.edit().putString("line", Driver.getLine()).apply();
+                            SP.edit().putString("type", "Driver").apply();
+                            SP.edit().putString("name", Driver.getName()).apply();
+                            SP.edit().putString("PIN", Driver.getPIN()).apply();
+                            move();
+
+                        } else toastMessage("This number is not registered, you need to sign up");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        toastMessage("Something went wrong!");
+                    }
+                });
+    }
+
+
+    public void move() {
+        Intent i = new Intent(this, verification.class);
         startActivity(i);
+
+    }
+
+    public void toastMessage(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+
     }
 
 
-
-
-
-    private void getLocationPermission(){
-        String[] permission= {Manifest.permission.ACCESS_FINE_LOCATION};
-        if(ContextCompat.checkSelfPermission(this,permission[0])== PackageManager.PERMISSION_GRANTED){
-            mLocationPermissionGranted=true;
-        }
-        else {
-            ActivityCompat.requestPermissions(this,permission,1234);
+    private void getLocationPermission() {
+        String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this, permission[0]) == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this, permission, 1234);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        mLocationPermissionGranted=false;
-        switch (requestCode){
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
             case 1234:
-                if(grantResults.length>0 ){
+                if (grantResults.length > 0) {
 
-                    for (int i=0;i<grantResults.length;i++){
-                        if(grantResults[0]!= PackageManager.PERMISSION_GRANTED){
-                            mLocationPermissionGranted=false;
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                             System.exit(0);
                         }
                     }
-                    mLocationPermissionGranted=true;
-
+                    mLocationPermissionGranted = true;
                 }
         }
     }
