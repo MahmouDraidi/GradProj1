6dd38738d09879776324999359b84fdc12af6362
Mohammad B Obaid <mohamadobaid96@gmail.com>
2019-03-03 20:52:27 +0200

    receive notification (background & inApp )

---
+++ app/src/main/java/com/gradproj1/Notifications.java
@@ -0,0 +1,52 @@
+package com.gradproj1;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+public class Notifications extends FirebaseMessagingService {
+
+   int id = 0 ;
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+    public void  showNotification (String title , String message){
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.gradproj1.test";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification",notificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("cc");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+        NotificationCompat.Builder builder =new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_launcher_background)   // change it
+              //  .setAutoCancel(true)
+                .setContentInfo("info")
+                .setContentText(message);
+
+     //   NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        notificationManager.notify(new Random().nextInt(), builder.build());
+    }
+
+}
